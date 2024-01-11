ac9ad93f3b07a8ba4da15c8e9e6c6a4a96bc38a1
Diane Adjavon <adjavond@hhmi.org>
2024-01-11 13:44:20 -0500

    Add a model for inference
    Only loads the necessary models.

---
+++ src/starganv2/inference/model.py
@@ -0,0 +1,42 @@
+"""Reduces the model into just want is needed for inference."""
+from munch import Munch
+from os.path import join as ospj
+from starganv2.core.model import Generator, MappingNetwork, StyleEncoder
+from starganv2.core.checkpoint import CheckpointIO
+import torch
+
+
+class LatentInferenceModel(torch.nn.Module):
+    def __init__(
+        self, checkpoint_dir, img_size, style_dim, latent_dim, num_domains=6, w_hpf=0.0
+    ) -> None:
+        super().__init__()
+        generator = Generator(img_size, style_dim, w_hpf=w_hpf)
+        mapping_network = MappingNetwork(latent_dim, style_dim, num_domains=num_domains)
+
+        self.nets = torch.nn.ModuleDict({
+            "generator": generator,
+            "mapping_network": mapping_network,
+        })
+
+        self.checkpoint_io = CheckpointIO(
+            ospj(checkpoint_dir, "{:06d}_nets_ema.ckpt"),
+            data_parallel=False,
+            **self.nets,
+        )
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.nets.to(self.device)
+        self.latent_dim = latent_dim
+        self.style_dim = style_dim
+
+    def load_checkpoint(self, step):
+        self.checkpoint_io.load(step)
+    
+    def forward(self, x_src, y_trg):
+        z = torch.randn(x_src.size(0), self.latent_dim).to(self.device)
+        s = self.nets.mapping_network(z, y_trg)
+        x_fake = self.nets.generator(x_src, s)
+        return x_fake
+
+# TODO ReferenceInferenceModel
